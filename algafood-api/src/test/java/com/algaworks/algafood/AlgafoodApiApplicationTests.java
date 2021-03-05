542cdcec16e1dd6059f9e2d6ed091c67793c06f2
guiaol <arantesbhz@gmail.com>
2021-03-05 07:40:09 -0300

    Codigo fonte do projeto

---
+++ algafood-api/src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
@@ -0,0 +1,18 @@
+package com.algaworks.algafood;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AlgafoodApiApplicationTests {
+
+	
+	public AlgafoodApiApplicationTests() {
+		
+	}
+	
+	@Test
+	public void contextLoads() {
+	}
+
+}

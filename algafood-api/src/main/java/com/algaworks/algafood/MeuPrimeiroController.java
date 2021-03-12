5a6677e5e8f3e1a72ba7de037de4cd4ae71d1509
guiaol <arantesbhz@gmail.com>
2021-03-12 07:04:37 -0300

    Injetando dependências (beans Spring)

--- algafood-api/src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -4,11 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.algaworks.algafood.di.modelo.Cliente;
+import com.algaworks.algafood.di.service.AtivacaoClienteService;
+
 @Controller
 public class MeuPrimeiroController {
+	
+	private AtivacaoClienteService ativacaoClienteService;
+	
+	public MeuPrimeiroController(AtivacaoClienteService ativacaoClienteService) {
+		this.ativacaoClienteService = ativacaoClienteService;
+		
+		System.out.println("MeuPrimeiroController: " + ativacaoClienteService);
+	}
+	
 	@GetMapping("/hello")
 	@ResponseBody
 	public String hello() {
+		Cliente cliente = new Cliente("guilherme", "arantesbhz@gmail.com", "31993078177");
+		
+		ativacaoClienteService.ativar(cliente);
+		
 		return "Hello!!!!";
 	}
 

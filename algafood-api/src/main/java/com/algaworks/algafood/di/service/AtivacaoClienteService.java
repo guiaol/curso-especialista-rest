82b4d85fd127e14d0f1fc081b23f09c67882deb2
guiaol <arantesbhz@gmail.com>
2021-03-17 08:02:40 -0300

    Publicando e consumindo eventos customizados

--- algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,36 +1,21 @@
 package com.algaworks.algafood.di.service;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import com.algaworks.algafood.di.notificador.NivelUrgencia;
-import com.algaworks.algafood.di.notificador.Notificador;
-import com.algaworks.algafood.di.notificador.TipoDoNotificador;
 
-//@Component
+@Component
 public class AtivacaoClienteService {
 
-	@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 	@Autowired
-	private Notificador notificador;
-
-//	@PostConstruct
-	public void init() {
-		System.out.println("Init");
-	}
-	
-//	@PreDestroy
-	public void destroy() {
-		System.out.println("Destroy");
-	}
+	private ApplicationEventPublisher eventPublisher;
 	
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
+		
+		eventPublisher.publishEvent(new ClienteAtivadoEvent(cliente));
 
-		notificador.notificar(cliente, "Seu cadastro no seu sistema está ativo!");
 	}
 }
---
+++ algafood-api/src/main/java/com/algaworks/algafood/listener/NotificacaoService.java
@@ -0,0 +1,24 @@
+package com.algaworks.algafood.listener;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.di.notificador.NivelUrgencia;
+import com.algaworks.algafood.di.notificador.Notificador;
+import com.algaworks.algafood.di.notificador.TipoDoNotificador;
+import com.algaworks.algafood.di.service.ClienteAtivadoEvent;
+
+@Component
+public class NotificacaoService {
+
+	@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
+	@Autowired
+	private Notificador notificador;
+	
+	@EventListener
+	public void clienteAtivadoListener(ClienteAtivadoEvent event) {
+		System.out.println("Cliente " + event.getCliente().getNome() + " agora esta ativo!");
+	}
+	
+}

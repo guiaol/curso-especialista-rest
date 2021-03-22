6b56ad60fa42edb12cefcd91fdbad4558c8f4b9f
guiaol <arantesbhz@gmail.com>
2021-03-22 07:49:22 -0300

    Adicionando JPA e configurando o Data Source

--- algafood-api/src/main/java/com/algaworks/algafood/di/notificador/NivelUrgencia.java
+++
@@ -1,7 +0,0 @@
-package com.algaworks.algafood.di.notificador;
-
-public enum NivelUrgencia {
-
-	URGENTE,
-	SEM_URGENCIA;
-}
--- algafood-api/src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
+++ algafood-api/src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
@@ -1,16 +1,14 @@
 package com.algaworks.algafood;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class AlgafoodApiApplicationTests {
 
-	
-	public AlgafoodApiApplicationTests() {
-		
-	}
-	
 	@Test
 	public void contextLoads() {
 	}

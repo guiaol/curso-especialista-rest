3c23fde1b87649c6ea4694ec6a1a716d83888b00
guiaol <arantesbhz@gmail.com>
2021-04-03 08:59:49 -0300

    Implementando Content Negotiation para retornar Json/Xml

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +14,21 @@ import com.algaworks.algafood.domain.repository.CozinhaRepository;
 //@Controller
 //@ResponseBody
 @RestController
-@RequestMapping("/cozinhas")
+@RequestMapping(value = "/cozinhas") // , produces = MediaType.APPLICATION_JSON_VALUE)
 public class CozinhaController {
 
 	@Autowired
 	public CozinhaRepository cozinhaRepository;
 	
-	@GetMapping
-	public List<Cozinha> listar() {
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Cozinha> listar1() {
+		System.out.println("Listar 1");
+		return cozinhaRepository.listar();
+	}
+	
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	public List<Cozinha> listar2() {
+		System.out.println("Listar 2");
 		return cozinhaRepository.listar();
 	}
 	
